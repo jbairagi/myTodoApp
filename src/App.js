4ded8030d1d19afcaaeecc0c1688df0846500c38
Jayesh <jbairagi@grepruby.com>
2017-09-28 00:41:37 +0530

    Updated: Successfully adding and deleting tasks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import React from 'react';
 
 class ListItems extends React.Component{
+  constructor(props){
+    super(props);
+    this.removeTask = this.removeTask.bind(this);
+  }
+
+  removeTask(e) {
+		this.props.removeTask(this.props.taskId);
+  	e.preventDefault();
+	}
+  updateTask(e) {
+		e.preventDefault();
+		// this.props.remove(this.props.taskId);
+		// return;
+	}
+
   render(){
     return(
       <div>
-				{this.props.task} <button type="button">Update</button> <button type="button">Delete</button>
+				{this.props.task}
+        <button type="button" onClick={this.updateTask}>Update</button>
+        <button type="button" onClick={this.removeTask}>Delete</button>
 			</div>
     );
   }
 }
 
 class TODOList extends React.Component{
-  render(){
-    const taskRows = [];
+  constructor(props){
+    super(props);
+    this.removeTask = this.removeTask.bind(this);
+  }
 
-    this.props.tasks.forEach((task) => {
-      taskRows.push(<ListItems task={task.task} key={task.id} />);
-    });
+  removeTask(taskId){
+    this.props.removeTask(taskId);
+		return;
+  }
+
+  render() {
+    const taskRows = this.props.tasks.map(function (task) {
+			return (
+        <ListItems task={task.task} taskId={task.id} removeTask={this.removeTask} key={task.id} />
+			);
+		},this);
+
     return (
       <div>
         {taskRows}
@@ -30,16 +58,16 @@ class TODOAddForm extends React.Component{
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   handleSubmit(e) {
-    this.props.onTaskSubmit(e.target.value);
-    console.log(e.target.value);
+    this.props.onTaskSubmit(this.todoInput.value);
     e.preventDefault();
 	}
 
   render(){
     return(
       <form onSubmit={this.handleSubmit}>
-        <input type="text" value={this.props.tasks} placeholder="Enter new task here" />
+        <input type="text" ref={(input) => this.todoInput = input} placeholder="Enter new task here" />
         <input type="submit" value="Add" />
       </form>
     );
@@ -51,25 +79,33 @@ class TODOApp extends React.Component {
     super(props);
     this.state = {
       data: [
-        {id: '1', task: 'learn React'},
-        {id: '2', task: 'apply React'}
+        {id: 1, task: 'learn React'},
+        {id: 2, task: 'apply React'}
       ]
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTaskRemoval = this.handleTaskRemoval.bind(this);
+    this.handleTaskUpdation = this.handleTaskUpdation.bind(this);
   }
 
-  handleTaskRemoval(nodeId) {
+  handleTaskRemoval(taskId) {
 		let data = this.state.data;
+		data = data.filter(function (e) {
+			return e.id !== taskId;
+		});
+		this.setState({data : data});
+	}
 
-		// data = data.filter(function (e) {
-		// 	return e.id !== nodeId;
-		// });
+  handleTaskUpdation(taskId) {
+		let data = this.state.data;
+    //
+    //
 		this.setState({data : data});
 	}
 
   handleSubmit(task) {
 		let data = this.state.data;
-		let id = data.length + 2 ;
+		let id = data.length + 1 ;
 		data = data.concat([{id, task}]);
 		this.setState({data : data});
 	}
@@ -78,7 +114,7 @@ class TODOApp extends React.Component {
     return (
       <div>
         <TODOAddForm onTaskSubmit = {this.handleSubmit}/>
-        <TODOList tasks={this.state.data} removeTask={this.handleTaskRemoval} />
+        <TODOList tasks={this.state.data} updateTask={this.handleTaskUpdation} removeTask={this.handleTaskRemoval} />
       </div>
     );
   }
