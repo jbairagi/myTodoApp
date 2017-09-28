d357d13336c36b6e67e74e53710f9d1fb9e319c9
Jayesh <jbairagi@grepruby.com>
2017-09-28 11:50:05 +0530

    Seperating Add form component from main file

---
+++ src/TODOAddForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class TODOAddForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    this.props.onTaskSubmit(this.todoInput.value);
+    e.preventDefault();
+	}
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" ref={(input) => this.todoInput = input} placeholder="Enter new task here" />
+        <input type="submit" value="Add" />
+      </form>
+    );
+  }
+}
+
+export default TODOAddForm;
