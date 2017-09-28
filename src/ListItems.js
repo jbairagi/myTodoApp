94b4d1ee13cb67e2437af3bdc36257d3212ff083
Jayesh <jbairagi@grepruby.com>
2017-09-28 12:41:13 +0530

    Seperated ListItems components from main application

---
+++ src/ListItems.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class ListItems extends React.Component{
+  constructor(props){
+    super(props);
+    this.removeTask = this.removeTask.bind(this);
+    this.updateTask = this.updateTask.bind(this);
+  }
+
+  removeTask(e) {
+		this.props.removeTask(this.props.taskId);
+  	e.preventDefault();
+	}
+
+  updateTask(e) {
+		this.props.updateTask(this.todoUpdate.value, this.props.taskId);
+  	e.preventDefault();
+	}
+
+  render(){
+    return(
+      <div>
+        <br></br>
+				Task {this.props.taskId}: {this.props.task} <br></br>
+        <button type="button" onClick={this.removeTask}>Delete task</button>
+        <form onSubmit={this.updateTask}>
+          <input type="submit" value="Update" />
+          <input type="text" ref={(input) => this.todoUpdate = input} placeholder="Update the task here" />
+        </form>
+			</div>
+    );
+  }
+}
+
+export default ListItems;
