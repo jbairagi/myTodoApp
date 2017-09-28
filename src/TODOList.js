7e22a1532c8aee3bf33eca9eadddb87f244f5936
Jayesh <jbairagi@grepruby.com>
2017-09-28 12:41:40 +0530

    Seperated TODOList component from main application

---
+++ src/TODOList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ListItems from './ListItems';
+
+class TODOList extends React.Component{
+  constructor(props){
+    super(props);
+    this.removeTask = this.removeTask.bind(this);
+    this.updateTask = this.updateTask.bind(this);
+  }
+
+  removeTask(taskId){
+    this.props.removeTask(taskId);
+		return;
+  }
+  updateTask(task, taskId){
+    this.props.updateTask(task, taskId);
+		return;
+  }
+
+  render() {
+    const taskRows = this.props.tasks.map(function (task) {
+			return (
+        <ListItems task={task.task} taskId={task.id} removeTask={this.removeTask} updateTask={this.updateTask} key={task.id} />
+			);
+		},this);
+
+    return (
+      <div>
+        {taskRows}
+      </div>
+    );
+  }
+}
+
+export default TODOList;
