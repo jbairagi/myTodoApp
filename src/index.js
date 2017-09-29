9f391d09d801c698734f18868736f60df5f433f8
Jayesh <jbairagi@grepruby.com>
2017-09-29 15:34:48 +0530

    removed index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import TODOApp from './TODOApp';
 import registerServiceWorker from './registerServiceWorker';
 
