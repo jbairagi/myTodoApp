e0685f021b198399be5419bce5adcaa077cb148e
Jayesh <jbairagi@grepruby.com>
2017-09-29 13:52:09 +0530

    using stateless component

--- src/TODOList.js
+++ src/TODOList.js
@@ -1,34 +1,49 @@
 import React from 'react';
 import ListItems from './ListItems';
 
-class TODOList extends React.Component{
-  constructor(props) {
-    super(props);
-    this.removeTask = this.removeTask.bind(this);
-    this.updateTask = this.updateTask.bind(this);
-  }
-
-  removeTask(taskId) {
-    this.props.removeTask(taskId);
-  }
-
-  updateTask(task, taskId) {
-    this.props.updateTask(task, taskId);
-  }
-
-  render() {
-    const taskRows = this.props.tasks.map(function (task) {
-			return (
-        <ListItems task={task.task} taskId={task.id} removeTask={this.removeTask} updateTask={this.updateTask} key={task.id} />
-			);
-		},this);
-
+function TODOList(props){
+  const taskRows = props.tasks.map((task) => {
     return (
-      <ul className="list-group">
-				{taskRows}
-			</ul>
+      <ListItems task={task.task} taskId={task.id} removeTasko={props.removeTask} updateTask={props.updateTask} key={task.id} />
     );
-  }
+	},this);
+
+  return (
+    <ul className="list-group">
+			{taskRows}
+		</ul>
+  );
 }
 
 export default TODOList;
+
+
+// class TODOList extends React.Component{
+//   constructor(props) {
+//     super(props);
+//     this.removeTask = this.removeTask.bind(this);
+//     this.updateTask = this.updateTask.bind(this);
+//   }
+//
+//   removeTask(taskId) {
+//     this.props.removeTask(taskId);
+//   }
+//
+//   updateTask(task, taskId) {
+//     this.props.updateTask(task, taskId);
+//   }
+//
+//   render() {
+//     const taskRows = this.props.tasks.map(function (task) {
+// 			return (
+//         <ListItems task={task.task} taskId={task.id} removeTasko={this.removeTask} updateTask={this.updateTask} key={task.id} />
+// 			);
+// 		},this);
+//
+//     return (
+//       <ul className="list-group">
+// 				{taskRows}
+// 			</ul>
+//     );
+//   }
+// }
