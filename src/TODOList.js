f44cc85b547d43886f05a382862857e2896c2a4e
Jayesh <jbairagi@grepruby.com>
2017-10-02 20:03:18 +0530

    for future reference stateful approach

--- src/TODOList.js
+++ src/TODOList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ListItems from './ListItems';
 
-function TODOList(props){
+export default function TODOList(props) {
   const taskRows = props.tasks.map((task) => {
     return (
       <ListItems task={task.task} taskId={task.id} removeTasko={props.removeTask} updateTask={props.updateTask} key={task.id} />
@@ -15,7 +15,6 @@ function TODOList(props){
   );
 }
 
-export default TODOList;
 
 // For future reference
 //
