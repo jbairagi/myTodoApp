e6390c0b092192bec816ca13819630c052213582
Jayesh <jbairagi@grepruby.com>
2017-10-04 20:35:56 +0530

    using fetchAPI to get project titles as todos

--- src/components/TODOApp.js
+++ src/components/TODOApp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TODOAddForm from './../containers/TODOAddForm';
-import ShowTodoList from './../containers/ShowTodoList';
+import TODOList from './TODOList';
 import AppHead from './AppHead';
 import './../App.css';
 
@@ -10,7 +10,7 @@ const TODOApp = () => (
       <div className="col-md-8 col-md-offset-2">
         <AppHead />
         <TODOAddForm />
-        <ShowTodoList />
+        <TODOList />
       </div>
     </div>
   </div>
--- src/components/TODOList.js
+++ src/components/TODOList.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import ListItems from './ListItems';
+import {connect} from 'react-redux';
+import { addTodo } from '../actions'
 
-const TODOList = ({todos}) => {
-  console.log(todos);
-  return (
-    <ul className="list-group">
-      {
-        todos.map(task => <ListItems task={task.text} taskId={task.id} key={task.id} />)
-      }
-    </ul>
-  );
+class TODOList extends React.Component{
+
+  componentDidMount(){
+    fetch(`http://localhost:3000/projects`, { 
+      method: 'post',  
+      headers: {  
+        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"  
+      },  
+      body: 'username=cvbnm&password=cvbnm'  
+    })
+    .then(res => res.json())
+    .then(tasks => {
+      tasks.forEach(function(element) {
+        this.props.dispatch(addTodo(element.title))
+      }, this);
+    })
+    .catch(function (error) {  
+      console.log('Request failed', error);  
+    });
+  }
+
+  render(){
+    return (
+      <ul className="list-group">
+        {
+          this.props.todos.map(task => <ListItems task={task.text} taskId={task.id} key={task.id} />)
+        }
+      </ul>
+    );
+  }
 }
 
-export default TODOList
+
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  }
+}
+  
+export default connect(mapStateToProps)(TODOList);
+
+
+
+// fetch(`/projects`, { 
+//   method: 'post',  
+//   headers: {  
+//     "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"  
+//   },  
+//   body: 'username=cvbnm&password=cvbnm'  
+// }).then(res => res.json())
+// .catch(function (error) {  
+//   console.log('Request failed', error);  
+// });
