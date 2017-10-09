cf7ee5314ee0f0a8c438e7440c406b68e7b10b0f
Jayesh <jbairagi@grepruby.com>
2017-10-09 11:27:12 +0530

    created a login page

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,70 +1,46 @@
 import React from 'react';
-import './../App.css';
 import TODOApp from './../components/TODOApp';
 
 class loginPage extends React.Component{
-  componentDidMount() {
-    this.setState({ showModal: true });
-  }
-
-  close() {
-    this.setState({ showModal: false });
-  }
-
+  
   render(){
-    let user
+    let uname
     let pass
     return(
-      <div>
-        <div show={this.state.showModal} onHide={this.close} className="modal fade in" role="dialog">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <button type="button" className="close" data-dismiss="modal">&times;</button>
-                <h4 className="modal-title">Login</h4>
-              </div>
-              <div className="modal-body">
-                {/* <form role="form" method="POST" action=""> */}
-                <form
-                  onSubmit={e => {
-                    e.preventDefault()
-                    if (!user.value.trim() || !pass.value.trim()) {
-                      return
-                    }
-                    // console.log(user.value +' ' + pass.value);
-                  // routing  and validation will be done here
-                    
-                  }}
-                  >
-                  <input type="hidden" name="_token" value="" /> 
-                  <div className="form-group">
-                    <label className="control-label">Username</label>
-                    <div>
-                      <input type="text" className="form-control input-lg" required ref={node => {user = node}} placeholder="Username" />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <label className="control-label">Password</label>
-                    <div>
-                      <input type="password" className="form-control input-lg" required ref={node => {pass = node}} placeholder="Password" />
-                    </div>
-                  </div>
-                  <div className="modal-footer">
-                    <input type="submit" onClick={this.close} className="btn btn-default" data-dismiss="modal" value="Submit" />
-                  </div>
-                </form>
+      <div className="container add-top-margin">
+        <div className="row">
+          <div className="col-md-8 col-md-offset-2">
+          <form className="form-horizontal"  
+            onSubmit={e => {
+              e.preventDefault()
+              if (!uname.value.trim() || !pass.value.trim()) {
+                return
+              }
+              console.log(uname.value + ' ' + pass.value);
+            }}
+          >
+            <div className="form-group">
+              <label className="control-label col-sm-2" htmlFor="username">Username:</label>
+              <div className="col-sm-10">
+                <input type="text" className="form-control" required ref={node => {uname = node}} id="username" placeholder="Usename" />
               </div>
             </div>
+            <div className="form-group">
+              <label className="control-label col-sm-2" htmlFor="pwd">Password:</label>
+              <div className="col-sm-10"> 
+                <input type="password" className="form-control" id="pwd" required ref={node => {pass = node}} placeholder="Password" />
+              </div>
+            </div>
+            <div className="form-group"> 
+              <div className="col-sm-offset-2 col-sm-10">
+                <button type="submit" className="btn btn-default">Login</button>
+              </div>
+            </div>
+          </form>
           </div>
-        </div>
-
-        <div>
-          <TODOApp />
         </div>
       </div>
     );
   }
 }
-
-
 export default loginPage;
