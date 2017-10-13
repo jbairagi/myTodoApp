c42d47dd9227384d6bb8758ca6dec2693a790271
Jayesh <jbairagi@grepruby.com>
2017-10-13 15:48:58 +0530

    adding route dependencies and imports

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { browserHistory } from 'react-router'
 import todoApp from './reducers'
-import HomePage from './components/homePage'
+import Routes from './components/routes';
 import './App.css';
 
 let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 render(
   <Provider store={store}>
-    <HomePage />
+    <Routes />
   </Provider>,
   document.getElementById('root')
 )
